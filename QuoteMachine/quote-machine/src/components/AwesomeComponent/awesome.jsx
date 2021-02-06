c6f63680a4ade26901de0a26cc9a5d5b2d430a03
mstolovich <mstolovich@gmail.com>
2021-02-06 16:58:40 +0200

    [WIP] added logic

--- QuoteMachine/quote-machine/src/components/AwesomeComponent/awesome.jsx
+++ QuoteMachine/quote-machine/src/components/AwesomeComponent/awesome.jsx
@@ -1,5 +1,12 @@
 import { React } from "react";
 
 const AwesomeComponent = (props) => {
-  return <div>Hello Max!</div>;
+  const [todos, setToDos] = useState([
+    { id: 1, completed: false, tittle: "Купить хлеб" },
+    { id: 2, completed: false, tittle: "Купить масло" },
+    { id: 3, completed: false, tittle: "Купить молоко" },
+    { id: 4, completed: false, tittle: "Купить молоко" },
+  ]);
+
+  return <div>Сделать: {todos[0].tittle}</div>;
 };
